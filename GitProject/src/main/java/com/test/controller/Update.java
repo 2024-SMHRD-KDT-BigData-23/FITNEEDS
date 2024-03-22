25fb3f0283031a1f6f362b58b172716c58ad13d4
Nodu <Nodu00719@gmail.com>
2024-03-22 15:22:07 +0900

    업데이트

--- GitProject/src/main/java/com/test/controller/Update.java
+++ GitProject/src/main/java/com/test/controller/Update.java
@@ -3,8 +3,16 @@ package com.test.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.test.database.DAO;
+import com.test.model.MemberVO;
+
 public class Update implements Command {
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		return null;
+		DAO dao = new DAO();
+
+		MemberVO membervo = (MemberVO) request.getSession().getAttribute("member");
+		String mem_id = membervo.getMem_id();
+
+		return "redirect:/fitupdateForm.do";
 	}
 }
