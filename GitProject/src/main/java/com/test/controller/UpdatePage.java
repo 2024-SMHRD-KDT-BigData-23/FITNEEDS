6b980171632315edb4a2c5521673389287ef4380
Nodu <Nodu00719@gmail.com>
2024-03-22 15:30:01 +0900

    수정

--- GitProject/src/main/java/com/test/controller/UpdatePage.java
+++ GitProject/src/main/java/com/test/controller/UpdatePage.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.test.database.DAO;
 import com.test.model.MemberVO;
 
-public class Update implements Command {
+public class UpdatePage implements Command {
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		DAO dao = new DAO();
 
