5984eff0373964b7844a091b4f6bef25ca5e364a
Nodu <Nodu00719@gmail.com>
2024-03-30 17:22:12 +0900

    최종 수정

--- GitProject/src/main/webapp/assets/js/check_update.js
+++ GitProject/src/main/webapp/assets/js/check_update.js
@@ -1,14 +1,8 @@
 function validateForm() {
     var password = document.forms["profileForm"]["mem_pw"].value;
-    var newPassword = document.forms["profileForm"]["mem_newpw"].value;
 
     if (password === "") {
         alert("비밀번호를 입력해 주세요.");
-        return false;
-    }
-
-    if (newPassword === "") {
-        alert("변경할 비밀번호를 입력해 주세요.");
         return false;
     }
 
