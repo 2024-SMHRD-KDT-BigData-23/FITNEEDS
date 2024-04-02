b424629eed526e793ca3b6442289b46f153191b5
Nodu <Nodu00719@gmail.com>
2024-04-02 09:17:53 +0900

    알림창 수정

--- GitProject/src/main/webapp/assets/js/check_ch.js
+++ GitProject/src/main/webapp/assets/js/check_ch.js
@@ -1,10 +1,13 @@
 function validateForm() {
-	var chDate = document.getElementById("exampleInputDate").value;
-	var weight = document.getElementById("exampleInputConfirmPassword2").value;
+        var chDate = document.getElementById("exampleInputDate").value;
+        var weight = document.getElementById("exampleInputConfirmPassword2").value;
 
-	if (chDate === "" || weight === "") {
-		alert("날짜와 체중을 입력해주세요.");
-		return false;
-	}
-	return true;
-}
+        if (chDate === "" || weight === "") {
+            alert("날짜와 체중을 입력해주세요.");
+            return false;
+        }
+
+        // 성공적으로 완료되었다는 알림 표시
+        alert("성공적으로 완료되었습니다.");
+        return true;
+    }
